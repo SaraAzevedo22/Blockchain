5083e1a0bf7444e87c7e256f9791eaf968a9d114
André Tse <48758826+andretse77@users.noreply.github.com>
2022-05-11 23:55:44 +0100

    Add files via upload
    KademliaNode developing

---
+++ src/main/java/p2p/KademliaNode.java
@@ -0,0 +1,14 @@
+package p2p;
+
+public class KademliaNode {
+    String guid;
+    String ipAdress;
+    
+
+    public KademliaNode(String id, String adress) {
+        this.guid = id;
+        this.ipAdress = adress;
+
+    }
+
+}
