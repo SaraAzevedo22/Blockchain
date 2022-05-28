70d49d43b515cc74c914f5f80f278047eab81e5f
marcialeite24 <38685980+marcialeite24@users.noreply.github.com>
2022-05-28 22:21:03 +0100

    Wallet signature

--- src/main/java/p2p/Wallet.java
+++ src/main/java/p2p/Wallet.java
@@ -1,10 +1,15 @@
 package p2p;
 
+import Blockchain.Config;
+
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class Wallet {
-    private String privKey;
-    private String pubKey;
+    private String privateKey;
+    private String publicKey;
     private String id;
 
     public Wallet() {
@@ -18,13 +23,31 @@ public class Wallet {
             KeyPair kp = kpg.generateKeyPair();
             PrivateKey privKey = kp.getPrivate();
             PublicKey pubKey = kp.getPublic();
-
+            byte[] encodedPrivateKey = privKey.getEncoded();
+            byte[] encodedPublicKey = pubKey.getEncoded();
+            //this.privateKey= new String(Base64.encode(encodedPrivateKey));
+            //this.publicKey= new String(Base64.encode(encodedPublicKey));
 
         } catch (NoSuchAlgorithmException e) {
             System.out.println("Exception thrown : " + e);
         }
     }
 
+    public byte[][] signMessage(String message) throws NoSuchAlgorithmException, SignatureException, InvalidKeySpecException, InvalidKeyException {
+        Signature signature = Signature.getInstance("RSA");
 
-    
+        byte[] data = Base64.getDecoder().decode((privateKey.getBytes()));
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        signature.initSign(fact.generatePrivate(spec));
+
+        byte[] messageBytes = Config.calculateSHA256(publicKey.concat(String.valueOf(message))).getBytes();
+        signature.update(messageBytes);
+
+        byte[] digitalSignature = signature.sign();
+        
+        return new byte[][]{messageBytes,digitalSignature};
+    }
+
+
 }
