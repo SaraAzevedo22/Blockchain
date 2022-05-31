d5974328df3ec1a019eef6d6e3b8b337ebb00e61
André Tse <48758826+andretse77@users.noreply.github.com>
2022-05-31 11:02:57 +0100

    Routing table

--- src/main/java/p2p/KademliaNode.java
+++ src/main/java/p2p/KademliaNode.java
@@ -2,9 +2,38 @@ package p2p;
 
 public class KademliaNode {
     Node node;
-    KademliaBucket[] buckets;
-    //Storage
+    KademliaRoutingTable routingTable;
+    String storage;
 
+    public KademliaNode(Node node, KademliaRoutingTable routingTable, String storage) {
+        this.node = node;
+        this.routingTable = routingTable;
+        this.storage = storage;
+    }
 
+    
 
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
+
+    public KademliaRoutingTable getRoutingTable() {
+        return routingTable;
+    }
+
+    public void setRoutingTable(KademliaRoutingTable routingTable) {
+        this.routingTable = routingTable;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
 }
--- src/main/java/p2p/KademliaRoutingTable.java
+++ src/main/java/p2p/KademliaRoutingTable.java
@@ -1,15 +1,22 @@
 package p2p;
 
+import java.util.Arrays;
+
 public class KademliaRoutingTable {
     Node currentNode;
     KademliaBucket[] kbuckets;
+    final int ID_LENGTH = 160;
 
-    public KademliaRoutingTable(Node currentNode, KademliaBucket[] kbuckets) {
+    public KademliaRoutingTable(Node currentNode) {
         this.currentNode = currentNode;
-        this.kbuckets = kbuckets;
+        this.initialize();
     }
 
-    
+    public void initialize() {
+        this.kbuckets = new KademliaBucket[ID_LENGTH];
+        for(int i = 0;i < ID_LENGTH; i++) kbuckets[i] = new KademliaBucket(i);
+    }
+
 
     public Node getCurrentNode() {
         return currentNode;
@@ -25,5 +32,13 @@ public class KademliaRoutingTable {
 
     public void setKbuckets(KademliaBucket[] kbuckets) {
         this.kbuckets = kbuckets;
+    }
+
+    @Override
+    public String toString() {
+        return "KademliaRoutingTable{" +
+                "currentNode=" + currentNode +
+                ", kbuckets=" + Arrays.toString(kbuckets) +
+                '}';
     }
 }
